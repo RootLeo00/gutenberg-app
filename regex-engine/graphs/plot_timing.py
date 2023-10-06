d866fa96c3aa6c5ab222a1f4fd4e43fd1d12bebf
RootLeo00 <caterina.leonelli@gmail.com>
2023-10-06 12:52:04 +0200

    plot time, pattern length, text length ffrom pkl

--- regex-engine/graphs/plot_timing.py
+++ regex-engine/graphs/plot_timing.py
@@ -1,15 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+
+# plt.style.use('style.mplstyle')
+
 def plot_timing(file, x, y):
-    #read the pickle file (file)
-    df = pd.read_pickle(file)
-    #plot with matplot lib a lineplot
-    sns.set(style="darkgrid")
-    # Plot the responses for different events and regions
     
-    sns.lineplot(x=x, y=y, data=df)
-    #show
+    listdf=[]
+    name=[]
+    fig=plt.figure(figsize=(9, 9))
+    plt.subplots_adjust(left=0.25, bottom=0.15)
+    print("plotting: ", file)
+    i=0
+    for f in file:
+        listdf.append(pd.read_pickle(f))
+        print(listdf[i])
+
+        #retrieve engine name and pattern
+        tmp=f.split("/")[-1]
+        name.append(tmp.split("_")[1])
+        print("name", name)
+
+        #plot xtick every 10 elements 
+        df=listdf[i]
+        plt.scatter(df[x][::5], df[y][::5], label=name[i].upper(), marker='o')
+        plt.plot(df[x], df[y])
+        i+=1
+
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    plt.legend()
+    plt.xlabel(x, fontsize=12)
+    plt.ylabel(y, fontsize=12)
+
+    plt.savefig('./'+x+'_'+y+'.png')
     plt.show()
 
 
@@ -18,5 +42,7 @@ if __name__ == "__main__":
     # main()
 	# filename=sys.argv[1]
 	# pattern=sys.argv[2]
-	plot_timing("../output/output_kmp.pkl", "time_elapsed", "ncharacters")
-	# plot_timing("../output/output_kmp.pkl",  "time_elapsed","pattern_len")
+    file=["../output/output_radixtree_patternlength.pkl","../output/output_kmp_patternlength.pkl"]
+    plot_timing(file,  "pattern_len","time_elapsed")
+    file=["../output/output_radixtree_textlength.pkl","../output/output_kmp_textlength.pkl"]
+    plot_timing( file, "ncharacters", "time_elapsed")
